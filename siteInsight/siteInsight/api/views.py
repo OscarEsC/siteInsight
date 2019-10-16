6ae2db003353bcd1a3ce899ae0a8d3c1f94e42d2
OscarEsC <oscare_@outlook.com>
2019-10-16 09:52:18 -0500

    index para app web

--- siteInsight/siteInsight/api/views.py
+++ siteInsight/siteInsight/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from django.template import loader
+from django.shortcuts import render_to_response
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #template = loader.get_template("siteInsight/api/index.html")
+    #return HttpResponse(template.render())
+    return render_to_response('index.html')
